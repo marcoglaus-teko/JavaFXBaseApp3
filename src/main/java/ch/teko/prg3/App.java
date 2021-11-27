a3a090223f3971b2a51e0d11b1884e85357b00db
glausm <marco.glaus@protonmail.ch>
2021-11-27 05:18:17 +0100

    init

--- src/main/java/ch/teko/prg3/App.java
+++ src/main/java/ch/teko/prg3/App.java
@@ -17,13 +17,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"));
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
--- src/main/java/ch/teko/prg3/PrimaryController.java
+++ src/main/java/ch/teko/prg3/PrimaryController.java
@@ -1,12 +1,26 @@
 package ch.teko.prg3;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Button b1;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Button b3;
+
+    @FXML
+    void openBrowser(ActionEvent event) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        String url = "https://youtube.com";
+        rt.exec("open " + url);
     }
 }
